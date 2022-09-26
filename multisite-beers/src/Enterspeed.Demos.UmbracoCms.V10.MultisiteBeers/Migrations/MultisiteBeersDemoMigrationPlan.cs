aee5f98eb9913bdbbc92efef9a10c5c2ba23e900
Jesper Lundgreen Weber <jlw@enterspeed.com>
2022-09-26 12:17:33 +0200

    Added multisite-beers demo content and adjusted pipelines

---
+++ multisite-beers/src/Enterspeed.Demos.UmbracoCms.V10.MultisiteBeers/Migrations/MultisiteBeersDemoMigrationPlan.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace Enterspeed.Demos.UmbracoCms.V10.MultisiteBeers.Migrations
+{
+    public class MultisiteBeersDemoMigrationPlan : AutomaticPackageMigrationPlan
+    {
+        public MultisiteBeersDemoMigrationPlan()
+            : base("Enterspeed multisite beers tutorial content")
+        {
+        }
+    }
+}
