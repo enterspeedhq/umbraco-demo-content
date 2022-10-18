f2c01962e1ea674d2be1f85de6d8b5ef125ecf91
jesperweber <jlw@enterspeed.com>
2022-10-18 08:59:19 +0200

    Feature/multisite beers (#9)
    * Added multisite-beers demo content and adjusted pipelines
    
    * Updated multisite-beers version number
    
    * Updated umbraco medias and package.xml files for multisite beers
    
    * Updated migration to handle media items
    
    * Delete duplicate pipeline file
    
    * Bump version

---
+++ multisite-beers/src/Enterspeed.Demos.UmbracoCms.V9.MultisiteBeers/Migrations/Migration202210171606InitialPackageContent.cs
@@ -0,0 +1,34 @@
+﻿using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Strings;
+using Umbraco.Cms.Infrastructure.Migrations;
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace Enterspeed.Demos.UmbracoCms.V9.MultisiteBeers.Migrations
+{
+    public class Migration202210171606InitialPackageContent : PackageMigrationBase
+    {
+        public Migration202210171606InitialPackageContent(
+            IPackagingService packagingService,
+            IMediaService mediaService,
+            MediaFileManager mediaFileManager,
+            MediaUrlGeneratorCollection mediaUrlGenerators,
+            IShortStringHelper shortStringHelper,
+            IContentTypeBaseServiceProvider contentTypeBaseServiceProvider,
+            IMigrationContext context) : base(packagingService,
+            mediaService,
+            mediaFileManager,
+            mediaUrlGenerators,
+            shortStringHelper,
+            contentTypeBaseServiceProvider,
+            context)
+        {
+        }
+
+        protected override void Migrate()
+        {
+            ImportPackage.FromEmbeddedResource(GetType()).Do();
+        }
+    }
+}
---
+++ multisite-beers/src/Enterspeed.Demos.UmbracoCms.V9.MultisiteBeers/Migrations/MultisiteBeersDemoMigrationPlan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Umbraco.Cms.Core.Packaging;
+
+namespace Enterspeed.Demos.UmbracoCms.V9.MultisiteBeers.Migrations
+{
+    public class CustomPackageMigrationPlan : PackageMigrationPlan
+    {
+        public CustomPackageMigrationPlan() : base("Enterspeed multisite beers tutorial content")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<Migration202210171606InitialPackageContent>(new Guid("4FD681BE-E27E-4688-922B-29EDCDCB8A49"));
+        }
+    }
+}
