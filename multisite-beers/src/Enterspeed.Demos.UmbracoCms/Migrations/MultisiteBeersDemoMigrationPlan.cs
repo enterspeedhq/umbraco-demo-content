4cb34641961c4bbf0231749d25cb53253461f907
MHove89 <mathiashove02@gmail.com>
2023-04-07 23:09:12 +0200

    Prepping solution for merging codebases

--- multisite-beers/src/Enterspeed.Demos.UmbracoCms/Migrations/MultisiteBeersDemoMigrationPlan.cs
+++ multisite-beers/src/Enterspeed.Demos.UmbracoCms/Migrations/MultisiteBeersDemoMigrationPlan.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Umbraco.Cms.Core.Packaging;
 
-namespace Enterspeed.Demos.UmbracoCms.V9.MultisiteBeers.Migrations
+namespace Enterspeed.Demos.UmbracoCms.MultisiteBeers.Migrations
 {
     public class CustomPackageMigrationPlan : PackageMigrationPlan
     {
@@ -13,5 +13,5 @@ namespace Enterspeed.Demos.UmbracoCms.V9.MultisiteBeers.Migrations
         {
             To<Migration202210171606InitialPackageContent>(new Guid("4FD681BE-E27E-4688-922B-29EDCDCB8A49"));
         }
-    }
+    }    
 }
